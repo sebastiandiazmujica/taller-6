48c946dac812e187f6c0c2e53221e124b5e71708
SebastianMujica22 <42355577+SebastianMujica22@users.noreply.github.com>
2020-11-24 22:10:21 -0500

    Taller finalizado

---
+++ webapp/controller/DetailObjectNotFound.js
@@ -0,0 +1,7 @@
+sap.ui.define([
+	"./BaseController"
+], function (BaseController) {
+	"use strict";
+
+	return BaseController.extend("com.sap.ua.Gateway_171.controller.DetailObjectNotFound", {});
+});
---
+++ webapp/test/integration/AllJourneys.js
@@ -0,0 +1,20 @@
+// We cannot provide stable mock data out of the template.
+// If you introduce mock data, by adding .json files in your webapp/localService/mockdata folder you have to provide the following minimum data:
+// * At least 3 BusinessPartnerCollection in the list
+// * All 3 BusinessPartnerCollection have at least one RelatedContacts
+
+sap.ui.define([
+	"sap/ui/test/Opa5",
+	"./arrangements/Startup",
+	"./MasterJourney",
+	"./NavigationJourney",
+	"./NotFoundJourney",
+	"./BusyJourney"
+], function (Opa5, Startup) {
+	"use strict";
+	Opa5.extendConfig({
+		arrangements: new Startup(),
+		viewNamespace: "com.sap.ua.Gateway_171.view.",
+		autoWait: true
+	});
+});
